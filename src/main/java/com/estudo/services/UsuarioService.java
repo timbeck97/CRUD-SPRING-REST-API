d487af148c10da05570cb3c5d48d38a0e8a17102
Tim Beck <timbeck1997@hotmail.com>
2021-11-28 12:09:39 -0300

    Criado metodos para buscar usuario por ID

--- src/main/java/com/estudo/services/UsuarioService.java
+++ src/main/java/com/estudo/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.estudo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,10 @@ public class UsuarioService {
 	
 	public List<Usuario> buscaUsuarios(){
 		return usuarioResource.findAll();
+	}
+	public Usuario buscaUsuario(Long id) {
+		Optional<Usuario> usuario=usuarioResource.findById(id);
+		return usuario.get();
 	}
 	
 }
